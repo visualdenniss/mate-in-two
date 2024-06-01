8e7f759c673663a0a19254cda537b48cd2820d89
visualdenniss <visualdennis@gmail.com>
2024-06-01 17:39:00 +0200

    format

--- src/App.js
+++ src/App.js
@@ -1,99 +1,98 @@
-import React, { useState, useRef, useEffect } from 'react'
-import axios from 'axios'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Sidebar from './Components/Sidebar/Sidebar'
-import Home from './Pages/Home/Home'
-import Cats from './Pages/Cats/Cats'
-import About from './Pages/About/About'
-import Composers from './Pages/Composers/Composers'
-import Theme from './Components/Theme/Theme'
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Home from "./Pages/Home/Home";
+import Cats from "./Pages/Cats/Cats";
+import About from "./Pages/About/About";
+import Composers from "./Pages/Composers/Composers";
+import Theme from "./Components/Theme/Theme";
 
-import './App.css'
+import "./App.css";
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
 
+  const [fen, setFen] = useState(
+    "2R5/4bppk/1p1p3Q/5R1P/4P3/5P2/r4q1P/7K b - - 6 50",
+  );
+  const [puzzleId, setPuzzleId] = useState("");
+  const [puzzleAuthor, setPuzzleAuthor] = useState([]);
+  const [puzzleSourceName, setPuzzleSourceName] = useState("");
+  const [puzzleYear, setPuzzleYear] = useState("");
 
-    const [isLoading,setIsLoading] = useState(false)
+  const url = "https://api-mate-in-two.onrender.com/";
 
-    const [fen,setFen] = useState("2R5/4bppk/1p1p3Q/5R1P/4P3/5P2/r4q1P/7K b - - 6 50")
-    const [puzzleId, setPuzzleId] = useState('')
-    const [puzzleAuthor, setPuzzleAuthor] = useState([])
-    const [puzzleSourceName, setPuzzleSourceName] = useState('')
-    const [puzzleYear,setPuzzleYear] = useState('')
+  const getPuzzle = async () => {
+    setIsLoading(true);
+    const res = await axios.get(url);
+    const puzzleFen = res.data.fen;
+    const id = res.data.puzzleId;
+    const source = res.data.puzzleSource.name;
+    const year = res.data.puzzleSource.date.year;
+    const author = res.data.puzzleAuthor;
 
-    const url = 'https://api-mate-in-two.onrender.com/'
+    console.log(source);
 
-    
-    const getPuzzle = async () => {
-        setIsLoading(true)
-        const res = await axios.get(url)
-        const puzzleFen = res.data.fen
-        const id = res.data.puzzleId
-        const source = res.data.puzzleSource.name
-        const year = res.data.puzzleSource.date.year
-        const author = res.data.puzzleAuthor
+    setFen(puzzleFen);
+    setPuzzleId(id);
+    setPuzzleAuthor(author);
+    setPuzzleSourceName(source);
+    setPuzzleYear(year);
+    setIsLoading(false);
+  };
 
-        console.log(source);
+  const [darkMode, setDarkMode] = useState(true);
+  const body = document.body;
 
-        setFen(puzzleFen)
-        setPuzzleId(id)
-        setPuzzleAuthor(author)
-        setPuzzleSourceName(source)
-        setPuzzleYear(year)
-        setIsLoading(false)
-    }
+  const darkTheme = () => {
+    body.classList.remove("light");
+    body.classList.add("dark");
+    setDarkMode(true);
+  };
 
-    const [darkMode, setDarkMode] = useState(true)
-    const body = document.body 
+  const lightTheme = () => {
+    body.classList.remove("dark");
+    body.classList.add("light");
+    setDarkMode(false);
+  };
 
-    const darkTheme = () => {
-        body.classList.remove('light')
-        body.classList.add('dark')
-        setDarkMode(true)
-    }
+  useEffect(() => {
+    getPuzzle();
+  }, []);
 
-    const lightTheme = () => {
-        body.classList.remove('dark')
-        body.classList.add('light')
-        setDarkMode(false)
-    }
+  return (
+    <Router>
+      <div className="app">
+        <Sidebar></Sidebar>
+        <Theme darkTheme={darkTheme} lightTheme={lightTheme}></Theme>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Home
+                fen={fen}
+                setFen={setFen}
+                puzzleId={puzzleId}
+                setPuzzleId={setPuzzleId}
+                getPuzzle={getPuzzle}
+                puzzleAuthor={puzzleAuthor}
+                puzzleSource={puzzleSourceName}
+                puzzleYear={puzzleYear}
+                isLoading={isLoading}
+              ></Home>
+            }
+          ></Route>
+          <Route path="/cats" element={<Cats></Cats>}></Route>
+          <Route
+            path="/about"
+            element={<About darkMode={darkMode}></About>}
+          ></Route>
+          <Route path="/composers" element={<Composers></Composers>}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-    useEffect(()=>{
-        getPuzzle()
-    },[])
-
-
-    return (
-
-
-
-        <Router>
-        <div className='app'>
-            <Sidebar></Sidebar>
-            <Theme
-            darkTheme={darkTheme}
-            lightTheme={lightTheme}
-            ></Theme>
-            <Routes>
-            <Route exact path='/' 
-            element={<Home 
-            fen={fen} 
-            setFen={setFen} 
-            puzzleId={puzzleId} 
-            setPuzzleId={setPuzzleId} 
-            getPuzzle={getPuzzle} 
-            puzzleAuthor={puzzleAuthor}
-            puzzleSource={puzzleSourceName}
-            puzzleYear={puzzleYear}
-            isLoading={isLoading}></Home>}></Route>
-            <Route path='/cats' element={ <Cats></Cats>}></Route>
-            <Route path='/about' element={ <About
-            darkMode={darkMode}
-            ></About>}></Route>
-            <Route path='/composers' element={<Composers></Composers>}></Route>
-            </Routes>
-        </div>
-        </Router>
-    )
-}
-
-export default App
+export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,113 +1,120 @@
-import React, { useState, useRef, useEffect } from 'react'
-import ChessBoard from 'chessboardjsx'
+import React, { useState, useRef, useEffect } from "react";
+import ChessBoard from "chessboardjsx";
 import useMediaQuery from "../../Hooks/useMediaQuery";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
-import './Home.css'
-import { AiOutlinePlus } from 'react-icons/ai'
-import Loading from '../../Components/Loading/Loading'
-const Home = ({fen, puzzleId, getPuzzle,puzzleAuthor, puzzleSource, puzzleYear, isLoading}) => {
+import "./Home.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import Loading from "../../Components/Loading/Loading";
+const Home = ({
+  fen,
+  puzzleId,
+  getPuzzle,
+  puzzleAuthor,
+  puzzleSource,
+  puzzleYear,
+  isLoading,
+}) => {
+  // Media
 
-    // Media 
+  const isDesktop = useMediaQuery("(min-width: 800px)");
 
-    const isDesktop = useMediaQuery('(min-width: 800px)');
+  // Reveal Info / Solution
 
-    // Reveal Info / Solution 
+  const solutionRef = useRef();
+  const infoRef = useRef();
 
-    const solutionRef = useRef()
-    const infoRef = useRef()
+  const displaySolution = () => {
+    solutionRef.current.classList.toggle("solution-active");
+  };
 
-    const displaySolution = () => {
-        solutionRef.current.classList.toggle('solution-active')
-    }
+  const displayInfo = () => {
+    infoRef.current.classList.toggle("info-data-active");
+  };
 
-    const displayInfo = () => {
-        infoRef.current.classList.toggle('info-data-active')
-    }
+  const fadeOut = () => {
+    solutionRef.current.classList.remove("solution-active");
+  };
 
-    const fadeOut = () => {
-        solutionRef.current.classList.remove('solution-active')
-    }
+  const fadeOutInfo = () => {
+    infoRef.current.classList.remove("info-data-active");
+  };
 
-    const fadeOutInfo = () => {
-         infoRef.current.classList.remove('info-data-active')
-    }
-
-
-    return (
-        <div className='home'>
-            <div className="title">#02</div>
-            <div className="wrapper">
-                <div className="info-component">
-                    <div className="info-content" onClick={() => displayInfo()} 
-                    onMouseLeave={() => fadeOutInfo()}
-                    >
-                        <div ref={infoRef} className="info-data">
-                        <p className="author">
-                          <span>Author: </span>  {puzzleAuthor.map((author)=>(
-                              author
-                          ))}
-                        </p>
-                        <div className="puzzle-source">
-                            <span>Source:</span> {puzzleSource}, {puzzleYear}
-                        </div>
-                        </div>
-                        <span className='info-text'>
-                            Info
-                        </span>
-                        <span className="circle">
-                            <span className="info-icon">?</span>
-                        </span>
-                    </div>
-                </div>
-                <div className="board-component">
-                    {
-                        isLoading? <Loading></Loading>  : 
-                        <ChessBoard position={fen} width={isDesktop?400:300} ></ChessBoard>
-                    }
-                </div>
-                <div className="solution-component"
-                >
-                    <div className="solution-content" onClick={() => displaySolution()}
-                        onMouseLeave={() => fadeOut()}
-                    >
-                        <div className='solution-data'>
-                            <p ref={solutionRef} className='solution-data-text'>
-                                <a className='puzzle-solution-link' href={`https://yacpdb.org/#${puzzleId}`} target='_blank'>
-                                https://yacpdb.org/#{puzzleId}
-                                </a>
-                            </p>
-                        </div>
-                        <span className="border">
-                            <span className='solution-icon'>!</span>
-                        </span>
-                        <span className="solution-text">
-                            Solution
-                        </span>
-                    </div>
-                </div>
+  return (
+    <div className="home">
+      <div className="title">#02</div>
+      <div className="wrapper">
+        <div className="info-component">
+          <div
+            className="info-content"
+            onClick={() => displayInfo()}
+            onMouseLeave={() => fadeOutInfo()}
+          >
+            <div ref={infoRef} className="info-data">
+              <p className="author">
+                <span>Author: </span> {puzzleAuthor.map((author) => author)}
+              </p>
+              <div className="puzzle-source">
+                <span>Source:</span> {puzzleSource}, {puzzleYear}
+              </div>
             </div>
-
-            <div class="centerBox">
-
-                <div className="categoryWrapper">
-                    <h1>
-                        <AiOutlinePlus></AiOutlinePlus>
-                    </h1>
-                    <button>
-                        <span onClick={()=>getPuzzle()}>
-                            <span>
-                                <span>
-                                    Want more?
-                                </span>
-                            </span>
-                        </span>
-                    </button>
-                </div>
-
-            </div>
+            <span className="info-text">Info</span>
+            <span className="circle">
+              <span className="info-icon">?</span>
+            </span>
+          </div>
         </div>
-    )
-}
+        <div className="board-component">
+          {isLoading ? (
+            <Loading></Loading>
+          ) : (
+            <ChessBoard
+              position={fen}
+              width={isDesktop ? 400 : 300}
+            ></ChessBoard>
+          )}
+        </div>
+        <div className="solution-component">
+          <div
+            className="solution-content"
+            onClick={() => displaySolution()}
+            onMouseLeave={() => fadeOut()}
+          >
+            <div className="solution-data">
+              <p ref={solutionRef} className="solution-data-text">
+                <a
+                  className="puzzle-solution-link"
+                  href={`https://yacpdb.org/#${puzzleId}`}
+                  target="_blank"
+                >
+                  https://yacpdb.org/#{puzzleId}
+                </a>
+              </p>
+            </div>
+            <span className="border">
+              <span className="solution-icon">!</span>
+            </span>
+            <span className="solution-text">Solution</span>
+          </div>
+        </div>
+      </div>
 
-export default Home
+      <div class="centerBox">
+        <div className="categoryWrapper">
+          <h1>
+            <AiOutlinePlus></AiOutlinePlus>
+          </h1>
+          <button>
+            <span onClick={() => getPuzzle()}>
+              <span>
+                <span>Want more?</span>
+              </span>
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
