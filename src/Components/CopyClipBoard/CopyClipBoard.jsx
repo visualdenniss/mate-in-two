876380f2551df67047fd490e36486455cb7a6bd6
visualdennis <deniztasdelen@denizair.fritz.box>
2026-02-19 10:07:03 +0100

    update copyclipboard link

--- src/Components/CopyClipBoard/CopyClipBoard.jsx
+++ src/Components/CopyClipBoard/CopyClipBoard.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { LuLink } from "react-icons/lu";
-import { toast } from "sonner";
+import React from 'react';
+import { LuLink } from 'react-icons/lu';
+import { toast } from 'sonner';
 
 const CopyClipBoard = ({ id }) => {
   const copyToClipBoard = () => {
-    const url = `https://api-mate-in-two.onrender.com/${id}`;
+    const url = `https://mate-in-two.netlify.app/${id}`;
     navigator.clipboard
       .writeText(url)
       .then(() => {
-        toast("Link copied to clipboard: " + url, {
+        toast('Link copied to clipboard: ' + url, {
           duration: 2000,
         });
       })
       .catch((error) => {
-        console.error("Error copying to clipboard:", error);
-        toast("Failed to copy link to clipboard. Please try again.", {
+        console.error('Error copying to clipboard:', error);
+        toast('Failed to copy link to clipboard. Please try again.', {
           duration: 2000,
         });
       });
